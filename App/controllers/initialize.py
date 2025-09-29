033e744ddfe5036765ee1f3ffe39cd19ca30cb5b
Imuugi <virenderramnarine26@gmail.com>
2025-09-29 14:57:13 -0400

    fixes

--- App/controllers/initialize.py
+++ App/controllers/initialize.py
@@ -1,8 +1,10 @@
 from .user import create_user
 from App.database import db
-
-
+from resident import create_resident
+from driver import create_driver
 def initialize():
     db.drop_all()
     db.create_all()
     create_user('bob', 'bobpass')
+    create_resident( 'steve', 'steve123', 'Steven', 'Deonarine', '6798984' ,'Chaguanas', 'Savannah_Rd')
+    create_driver( 'rob', 'robpass', 'Robert', 'Francis', '87654321')
