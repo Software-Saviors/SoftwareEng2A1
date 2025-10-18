6f25bf75f1cf5aac67be8bccd32d9a800f53b827
SciPhi-of-n <jonathan.bruce@my.uwi.edu>
2025-10-18 22:28:26 +0000

    added tests for resident and driver

---
+++ App/tests/driver_test.py
@@ -0,0 +1,113 @@
+from App.models.Request import Request
+import os, tempfile, pytest, logging, unittest
+
+from App.main import create_app
+from App.database import db, create_db
+from App.models import driver
+from App.controllers import (
+    schedule_drive,
+    change_request_status,
+    view_requests_driver,
+    create_driver
+)
+
+
+LOGGER = logging.getLogger(__name__)
+
+'''Unit Testing'''
+
+class DriverUnitTests(unittest.TestCase):
+
+    def test_create_driver(self):
+        driver = create_driver(
+            username="testdriver",
+            password="testpassword",
+            fname="Test",
+            lname="Driver",
+            phone="1234567"
+        )
+        assert driver is not None
+        assert driver.id is not None
+        assert driver.username == "testdriver"
+        assert driver.fname == "Test"
+        assert driver.lname == "Driver"
+        assert driver.phone == "1234567"
+
+    def test_schedule_drive(self):
+        driver = create_driver(
+            username="testdriver",
+            password="testpassword",
+            fname="Test",
+            lname="Driver",
+            phone="1234567"
+        )
+        assert driver is not None
+        assert driver.id is not None
+        assert driver.username == "testdriver"
+        assert driver.fname == "Test"
+        assert driver.lname == "Driver"
+        assert driver.phone == "1234567"
+
+        driverlog = schedule_drive(
+            driver_id=driver.id,
+            city="TestCity",
+            licenseplate="TST1234"
+        )
+
+        assert driverlog is not None
+        assert driverlog.driver_id == driver.id
+        assert driverlog.city == "TestCity"
+        assert driverlog.licenseplate == "TST1234"
+
+    def test_change_request_status(self):
+        driver = create_driver(
+            username="testdriver",
+            password="testpassword",
+            fname="Test",
+            lname="Driver",
+            phone="1234567"
+        )
+
+        driverlog = schedule_drive(
+            driver_id=driver.id,
+            city="TestCity",
+            licenseplate="TST1234"
+        )
+        
+        request = Request(
+            resident_id=1,
+            drive_id=driverlog.id,
+            address="123 Test St"
+        )
+
+        request = change_request_status(request.id, 'accepted')
+        assert request.status == 'accepted'
+
+    def test_view_requests_driver(self):
+        driver = create_driver(
+            username="testdriver",
+            password="testpassword",
+            fname="Test",
+            lname="Driver",
+            phone="1234567"
+        )
+
+        driverlog = schedule_drive(
+            driver_id=driver.id,
+            city="TestCity",
+            licenseplate="TST1234"
+        )
+
+        request = Request(
+            resident_id=1,
+            drive_id=driverlog.id,
+            address="123 Test St"
+        )
+
+        requests = view_requests_driver(driver.id)
+        assert len(requests) == 1
+        assert requests[0].id == request.id
+        assert requests[0].resident_id == 1
+        assert requests[0].driver_id == driverlog.id
+        assert requests[0].status == 'pending'
+        assert requests[0].address == "123 Test St"
---
+++ App/tests/resident_test.py
@@ -0,0 +1,78 @@
+import os, tempfile, pytest, logging, unittest
+
+from App.main import create_app
+from App.database import db, create_db
+from App.models import resident
+from App.controllers import (
+    create_resident,
+    create_request,
+    view_inbox,
+    view_requests_resident
+)
+
+LOGGER = logging.getLogger(__name__)
+
+'''
+   Unit Tests
+'''
+
+class ResidentUnitTests(unittest.TestCase):
+    def test_create_resident(self):
+        resident = create_resident(
+            username="testresident",
+            password="testpassword",
+            fname="Test",
+            lname="Resident",
+            phone="7654321"
+        )
+        assert resident is not None
+        assert resident.id is not None
+        assert resident.username == "testresident"
+        assert resident.fname == "Test"
+        assert resident.lname == "Resident"
+        assert resident.phonenumber == "7654321"
+
+    def test_create_request(self):
+        resident = create_resident(
+            username="testresident",
+            password="testpassword",
+            fname="Test",
+            lname="Resident",
+            phone="7654321"
+        )
+
+        request= create_request(
+            resident_id=resident.id,
+            drive_id=1,
+            address="123 Test St"
+        )
+        
+        assert request is not None
+        assert request.id is not None
+        assert request.resident_id == resident.id
+        assert request.drive_id == 1
+        assert request.address == "123 Test St"
+
+    def test_view_inbox(self):
+        resident = create_resident(
+            username="testresident",
+            password="testpassword",
+            fname="Test",
+            lname="Resident",
+            phone="7654321"
+        )
+
+        inbox= view_inbox(resident.id)
+        assert inbox == []
+
+    def test_view_requests_resident(self):
+        resident = create_resident(
+            username="testresident",
+            password="testpassword",
+            fname="Test",
+            lname="Resident",
+            phone="7654321"
+        )
+
+        requests = view_requests_resident(resident.id)
+        assert requests == []
